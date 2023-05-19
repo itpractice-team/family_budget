51e7ca7359a1374466f1252bb6c9814d2e1d54fd
andrew <agatinet@gmail.com>
2023-05-19 15:37:23 +0300

    Форматирование кода

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,9 +4,7 @@ from backend.family_budget import settings
 class TestSettings:
     def test_settings(self):
         db_engine = settings.DATABASES["default"]["ENGINE"]
-        assert not settings.DEBUG, (
-            "Проверьте, что DEBUG в настройках Django выключен"
-        )
+        assert not settings.DEBUG, "Проверьте, что DEBUG в настройках Django выключен"
         assert (
             db_engine == "django.db.backends.postgresql"
         ), "Проверьте, что используете базу данных postgresql"
