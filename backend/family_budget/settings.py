034f8f7bcf9efbd450ff8d71fdf2c5ec5ed73598
andrew <agatinet@gmail.com>
2023-05-19 12:15:07 +0300

    Добавлены тесты для Pull request

--- backend/family_budget/settings.py
+++ backend/family_budget/settings.py
@@ -144,9 +144,14 @@ if DEBUG:
         'debug_toolbar',
     ]
 
-    INTERNAL_IPS = ['127.0.0.1', ]
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
     import mimetypes
+
     mimetypes.add_type("application/javascript", ".js", True)
 
     DEBUG_TOOLBAR_CONFIG = {
