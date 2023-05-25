3c39c6f6a6f9020983c045195441e813b9426c05
andrew <agatinet@gmail.com>
2023-05-25 20:09:41 +0300

    CORS headers

--- backend/family_budget/settings/prod.py
+++ backend/family_budget/settings/prod.py
@@ -1,5 +1,7 @@
 import os
 
+from corsheaders.defaults import default_headers
+
 from .base import *  # noqa
 
 DEBUG = False
@@ -8,6 +10,20 @@ ALLOWED_HOSTS += os.getenv("PRODUCTION_HOSTS", "backend").split(",")
 
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = list(
     map(lambda url: f'https://{url}', ALLOWED_HOSTS)
+)
+
+CORS_ALLOW_HEADERS = (
+    *default_headers,
+    "Sec-CH-UA",
+    "Sec-CH-UA-Arch",
+    "Sec-CH-UA-Bitness",
+    "Sec-CH-UA-Full-Version",
+    "Sec-CH-UA-Full-Version-List",
+    "Sec-CH-UA-Mobile",
+    "Sec-CH-UA-Model",
+    "Sec-CH-UA-Platform",
+    "Sec-CH-UA-Platform-Version",
+    "Referer",
 )
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
