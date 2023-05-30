9073eb84e8f3f82e680251df47c889f027bcb359
Daria Nikulina <daryanickulina@yandex.ru>
2023-05-31 00:26:46 +0400

    feat: adds spending block & Card

--- frontend/src/Pages/Budget/Budget.jsx
+++ frontend/src/Pages/Budget/Budget.jsx
@@ -4,6 +4,74 @@ import './Budget.scss';
 import SpendingPopup from '../../Components/SpendingPopup/SpendingPopup';
 import EarningPopup from '../../Components/EarningPopup/EarningPopup';
 import { toggleSpendingPopup, toggleEarningPopup } from '../../store/slices/togglePopupSlice';
+import SpendingCard from '../../Components/Card/Card';
+
+// mock data
+import cat from '../../Images/cat-ic-24.svg';
+import Vector from '../../Images/Vector.svg';
+
+const spend = [
+  {
+    id: 1,
+    header: 'Вкусняшки коту',
+    text: 'Sheba с креветками',
+    bank: 'Тинькофф',
+    amount: '-40₽',
+    categoryImg: cat,
+    bankLogo: Vector,
+    spending: true,
+  },
+  {
+    id: 2,
+    header: 'Вкусняшки коту',
+    text: 'Sheba с креветками',
+    bank: 'Тинькофф',
+    amount: '-40₽',
+    categoryImg: cat,
+    bankLogo: Vector,
+    spending: false,
+  },
+  {
+    id: 3,
+    header: 'Вкусняшки коту',
+    text: 'Sheba с креветками',
+    bank: 'Тинькофф',
+    amount: '-40₽',
+    categoryImg: cat,
+    bankLogo: Vector,
+    spending: true,
+  },
+  {
+    id: 4,
+    header: 'Вкусняшки коту',
+    text: 'Sheba с креветками',
+    bank: 'Тинькофф',
+    amount: '-40₽',
+    categoryImg: cat,
+    bankLogo: Vector,
+    spending: false,
+  },
+  {
+    id: 5,
+    header: 'Вкусняшки коту',
+    text: 'Sheba с креветками',
+    bank: 'Тинькофф',
+    amount: '-40₽',
+    categoryImg: cat,
+    bankLogo: Vector,
+    spending: true,
+  },
+  {
+    id: 6,
+    header: 'Вкусняшки коту',
+    text: 'Sheba с креветками',
+    bank: 'Тинькофф',
+    amount: '-40₽',
+    categoryImg: cat,
+    bankLogo: Vector,
+    spending: false,
+  },
+];
 
 export default function Budget() {
   const dispatch = useDispatch();
@@ -17,6 +85,7 @@ export default function Budget() {
   const handleSpendingClick = () => {
     dispatch(toggleSpendingPopup(true));
   };
+
   const handleEarningClick = () => {
     dispatch(toggleEarningPopup(true));
   };
@@ -24,21 +93,39 @@ export default function Budget() {
   const closeSpendingPopup = () => {
     dispatch(toggleSpendingPopup(false));
   };
+
   const closeEarningPopup = () => {
     dispatch(toggleEarningPopup(false));
   };
 
   return (
     <section className="budget">
-      <div className="budget__button-wrapper">
-        <button type="button" className="budget__add-button" onClick={handleSpendingClick}>
-          Добавить расход
-        </button>
+      <section className="budget__spending">
+        <div className="budget__filtration">
+          {/* filtration here */}
 
-        <button type="button" className="budget__add-button" onClick={handleEarningClick}>
-          Добавить доход
-        </button>
-      </div>
+          <div className="budget__button-wrapper">
+            <button type="button" className="budget__add-button" onClick={handleSpendingClick}>
+              Добавить расход
+            </button>
+
+            <button type="button" className="budget__add-button" onClick={handleEarningClick}>
+              Добавить доход
+            </button>
+          </div>
+        </div>
+
+        <div>{/* insert date */}</div>
+
+        {/* replace spend to props */}
+        <ul className="budget__spending-list">
+          {spend &&
+            spend.map((card) => {
+              return <SpendingCard {...card} key={card.id} />;
+            })}
+        </ul>
+      </section>
+
       {isSpendingPopupOpen && <SpendingPopup onClose={closeSpendingPopup} />}
 
       {isEarningPopupOpen && <EarningPopup onClose={closeEarningPopup} />}
