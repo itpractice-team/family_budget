c615fd924c2ea4c711abaef76c2cf8827ce4ec22
Masha Moreva <mashamiskova@yandex.ru>
2023-06-01 10:51:20 +0300

    changed-data-storage

--- frontend/src/store/slices/loginSlice.js
+++ frontend/src/store/slices/loginSlice.js
@@ -1,6 +1,7 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getCookie, setCookie } from '../../utils/cookies';
+import { getCookie, setCookie, deleteCookie } from '../../utils/cookies';
 
 export const loginUserAPI = async (userData) => {
   try {
@@ -35,6 +36,12 @@ export const loginUser = createAsyncThunk('user/login', async ({ username, passw
 const initialState = {
   // eslint-disable-next-line no-unneeded-ternary
   login: getCookie('token') ? true : false,
+  user: {
+    id: '',
+    first_name: '',
+    last_name: '',
+    avatar: null,
+  },
   loading: false,
   error: null,
 };
@@ -42,7 +49,14 @@ const initialState = {
 const loginSlice = createSlice({
   name: 'login',
   initialState,
-  reducers: {},
+  reducers: {
+    logoutUser(state) {
+      state.loading = false;
+      state.error = null;
+      deleteCookie('token');
+      state.login = false;
+    },
+  },
   extraReducers: (builder) => {
     // Обработка действий, созданных createAsyncThunk
     builder
@@ -52,9 +66,14 @@ const loginSlice = createSlice({
       })
       .addCase(loginUser.fulfilled, (state, action) => {
         state.loading = false;
-        // eslint-disable-next-line camelcase
+        const { id, first_name, last_name, avatar } = action.payload;
+        state.user = {
+          id,
+          first_name,
+          last_name,
+          avatar,
+        };
         const { auth_token } = action.payload;
-        // eslint-disable-next-line camelcase
         setCookie('token', auth_token);
         state.login = true;
       })
@@ -64,5 +83,7 @@ const loginSlice = createSlice({
       });
   },
 });
+
+export const { logoutUser } = loginSlice.actions;
 
 export const loginReducer = loginSlice.reducer;
