1d6de597ec6f14a9dafe5edd99fa77f924c51b46
Masha Moreva <mashamiskova@yandex.ru>
2023-06-06 22:52:50 +0300

    layout-account-component

--- frontend/src/Components/AccountSelect/AccountSelect.jsx
+++ frontend/src/Components/AccountSelect/AccountSelect.jsx
@@ -1,41 +1,66 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import './AccountSelect.scss';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button/Button';
+import { toggleAccountsPopup } from '../../store/slices/togglePopupSlice';
+import AccountsPopup from '../AccountsPopup/AccountsPopup';
 
-export default function AccountSelect({ handleOptionChange, selectedOption }) {
+export default function AccountSelect({ handleOptionChange, selectedOption, onClose }) {
+  const dispatch = useDispatch();
+
+  const isAccountsPopupOpen = useSelector((state) => state.popup.isAccountsPopupOpen);
+
   const options = ['Тинькофф', 'ВТБ', 'СБЕР'];
 
   const handleSelectOption = (value) => {
     handleOptionChange(value);
   };
 
+  const handleAccountsClick = (evt) => {
+    evt.preventDefault();
+    dispatch(toggleAccountsPopup(true));
+    onClose();
+  };
+
+  const closeAccountsPopup = () => {
+    dispatch(toggleAccountsPopup(false));
+    onClose();
+  };
+
   return (
-    <div className="account-select__list">
-      {options.map((option, index) => (
-        <label
-          key={index}
-          className="form__input-label form__input-label_radio"
-          htmlFor={`option${index}`}
-        >
-          {option}
-          <input
-            className="form__radio"
-            type="radio"
-            id={`option${index}`}
-            value={option}
-            checked={selectedOption === option}
-            onChange={() => handleSelectOption(option)}
+    <>
+      {isAccountsPopupOpen && <AccountsPopup onClose={closeAccountsPopup} />}
+      {isAccountsPopupOpen ? null : (
+        <div className="account-select__list">
+          <p className="account-select__list-title">Все счета</p>
+          {options.map((option, index) => (
+            <label
+              key={index}
+              className="form__input-label form__input-label_radio"
+              htmlFor={`option${index}`}
+            >
+              {option}
+              <input
+                className="form__radio"
+                type="radio"
+                id={`option${index}`}
+                value={option}
+                checked={selectedOption === option}
+                onChange={() => handleSelectOption(option)}
+              />
+            </label>
+          ))}
+          <Button
+            variant="secondary"
+            type="text"
+            text="Добавить/Редактировать"
+            size="medium"
+            extraClass="button__account"
+            onClick={handleAccountsClick}
           />
-        </label>
-      ))}
-      <Button
-        variant="secondary"
-        type="text"
-        text="Добавить/Редактировать"
-        size="medium"
-        extraClass="button__account"
-      />
-    </div>
+        </div>
+      )}
+    </>
   );
 }
