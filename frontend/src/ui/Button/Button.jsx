e36f341791a0c7dd249a536c80b689e861362cdd
Michael <m-silkov@inbox.ru>
2023-06-07 07:51:12 +0300

    feat: create switcher

--- frontend/src/ui/Button/Button.jsx
+++ frontend/src/ui/Button/Button.jsx
@@ -1,20 +1,29 @@
 import './Button.scss';
 
-export default function Button({ variant, type, text, size, disabled, extraClass, onClick }) {
+export default function Button({ variant, content, text, size, onClick, type, disabled, extraClass }) {
   const buttonClasses = `button ${extraClass} ${variant} ${size} ${disabled ? 'disabled' : ''} ${
-    type === 'icon' ? 'rounded' : ''
+    content === 'icon' ? 'rounded' : ''
   }`;
 
   return (
-    <button className={buttonClasses} disabled={disabled} onClick={onClick}>
-      {type === 'text' && <span className="button-text">{text}</span>}
-      {type === 'icon-text' && (
+    <button className={buttonClasses} disabled={disabled} onClick={onClick} type={type}>
+      {content === 'text' && <span className="button-text">{text}</span>}
+      {content === 'icon-text' && (
         <>
           <span className="button-icon"> </span>
           <span className="button-text">{text}</span>
         </>
       )}
-      {type === 'icon' && <span className="button-icon"> </span>}
+      {content === 'icon' && <span className="button-icon"> </span>}
     </button>
   );
 }
+
+Button.defaultProps = {
+  variant: 'primary',
+  content: 'text',
+  size: 'small',
+  onClick: () => {},
+  type: 'button',
+  disabled: false,
+};
