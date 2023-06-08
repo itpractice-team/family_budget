5b25172f7ae2cb2c6417c2fc741978d2203f614c
Masha Moreva <mashamiskova@yandex.ru>
2023-06-08 14:12:45 +0300

    added-custom-classes

--- frontend/src/Components/Loader/Loader.jsx
+++ frontend/src/Components/Loader/Loader.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import './Loader.scss';
 
-export default function Loader() {
-  return (
-    <div className="loader" />
-    // <div className="loader">
-    //   <div className="loader-circle">
-    //     <div className="loader-circle-border" />
-    //     <div className="loader-circle-gradient" />
-    //   </div>
-    // </div>
-  );
+export default function Loader({ extraClass }) {
+  return <div className={`loader ${extraClass}`} />;
 }
