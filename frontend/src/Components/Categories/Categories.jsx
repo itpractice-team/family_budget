ff1a78aa9f591d5a8028829e8932c8aee095ce2f
Masha Moreva <mashamiskova@yandex.ru>
2023-06-08 14:09:14 +0300

    changed-categories-component

--- frontend/src/Components/Categories/Categories.jsx
+++ frontend/src/Components/Categories/Categories.jsx
@@ -4,16 +4,16 @@ import Tab from '../Tab/Tab';
 import Button from '../../ui/Button/Button';
 
 export default function Categories() {
-
   const [activeTab, setActiveTab] = useState('Общие');
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
 
-  return <section className="categories">
-    <h2 className='categories__title'>Отображать категории</h2>
-    <div className="categories__tabs">
+  return (
+    <section className="categories">
+      <h2 className="categories__title">Отображать категории</h2>
+      <div className="categories__tabs">
         <Tab active={activeTab === 'Расходы'} value="Расходы" onClick={handleTabClick}>
           Расходы
         </Tab>
@@ -31,5 +31,6 @@ export default function Categories() {
         size="medium"
         extraClass="categories__button"
       />
-  </section>;
+    </section>
+  );
 }
