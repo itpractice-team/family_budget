c4ec048dfdaf28825b9715b24656f06812402de8
Masha Moreva <mashamiskova@yandex.ru>
2023-06-11 19:16:45 +0300

    added-overlay-for-select

---
+++ frontend/src/Components/Overlay/Overlay.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+export default function Overlay({ isOpen, onClose, children }) {
+  const overlayRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (overlayRef.current && !overlayRef.current.contains(event.target)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
+  return <div ref={overlayRef}>{isOpen && children}</div>;
+}
