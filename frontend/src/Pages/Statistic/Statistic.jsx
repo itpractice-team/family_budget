4feda77228cacaa4bf1a3590428bf32d166f7873
Daria Nikulina <daryanickulina@yandex.ru>
2023-06-11 13:50:53 +0400

    feat: adds general layout

--- frontend/src/Pages/Statistic/Statistic.jsx
+++ frontend/src/Pages/Statistic/Statistic.jsx
@@ -1,5 +1,30 @@
 import './Statistic.scss';
 
 export default function Statistic() {
-  return <section className="statistic" />;
+  return (
+    <section className="statistic">
+      <div className="statistic__overview statistic__block">
+        <p className="statistic__text">
+          Общее состояние по
+          {/* adds select */}
+        </p>
+      </div>
+
+      <div className="statistic__periods statistic__block">
+        <div className="statistic__filtration">
+          <p>stats filter</p>
+          {/* adds */}
+        </div>
+        <p>stats</p>
+      </div>
+
+      <div className="statistic__spending statistic__block">
+        <h2 className="statistic__header">Расходы по категориям</h2>
+      </div>
+
+      <div className="statistic__incoms statistic__block">
+        <h2 className="statistic__header">Доходы по категориям</h2>
+      </div>
+    </section>
+  );
 }
