b955c7baa12ac13b162e2a15171670bb75384213
Masha Moreva <mashamiskova@yandex.ru>
2023-06-12 21:05:18 +0300

    added-key

--- frontend/src/Components/Categories/Categories.jsx
+++ frontend/src/Components/Categories/Categories.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable react/no-array-index-key */
 import './Categories.scss';
 import Tab from '../Tab/Tab';
 
 export default function Categories({ arr, size, activeInit, onClick }) {
   return (
     <div className="categories-tabs">
-      {arr?.map((i) => {
+      {arr?.map((i, index) => {
         return (
-          <Tab active={activeInit === i.title} value={i.title} size={size} onClick={onClick}>
+          <Tab
+            key={index}
+            active={activeInit === i.title}
+            value={i.title}
+            size={size}
+            onClick={onClick}
+          >
             {i.title}
           </Tab>
         );
