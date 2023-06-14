793eea440da734b182c55155f6759a0a995bbfad
Michael <m-silkov@inbox.ru>
2023-06-14 19:14:47 +0300

    feat: create Checkbox markdown jsx

---
+++ frontend/src/ui/Checkbox/Checkbox.jsx
@@ -0,0 +1,30 @@
+import './Checkbox.scss';
+
+export default function Checkbox({
+  nameProp,
+  text,
+  icon,
+  isChecked,
+  onChange,
+  disabled,
+  extraClass,
+}) {
+  const checkboxClasses = `checkbox ${disabled ? 'disabled' : ''} ${extraClass}`;
+  const inputId = `checkbox-input-${Math.random().toString(36).slice(2, 9)}`; // Генерация уникального идентификатора
+  return (
+    <label className={checkboxClasses} htmlFor={inputId}>
+      <input
+        className="checkbox__input"
+        type="checkbox"
+        id={inputId}
+        name={nameProp}
+        checked={isChecked}
+        onChange={onChange}
+        disabled={disabled}
+      />
+
+      {icon && <img src={icon} alt="Ckeckbox-icon" className="checkbox__icon" />}
+      {text && <span className="checkbox__text">{text}</span>}
+    </label>
+  );
+}
