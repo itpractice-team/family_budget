2fa27c412cab56f9ff2ed4966cf8dd49f8c4eba7
Masha Moreva <mashamiskova@yandex.ru>
2023-06-15 20:00:02 +0300

    added-colors-in-consts

--- frontend/src/Components/Logo/Logo.jsx
+++ frontend/src/Components/Logo/Logo.jsx
@@ -1,14 +1,9 @@
-import './Logo.scss';
 import { NavLink } from 'react-router-dom';
 
 export default function Logo({ src }) {
   return (
     <NavLink to="/" className="logo">
       <img className="logo__image" src={src} alt="Логотип" />
-      {/* <div className="logo__text-content">
-        <h1 className="logo__title">InCoin</h1>
-        <p className="logo__subtitle">Отслеживание бюджета</p>
-      </div> */}
     </NavLink>
   );
 }
