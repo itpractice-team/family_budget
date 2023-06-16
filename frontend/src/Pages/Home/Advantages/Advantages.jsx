f2c6c259406d415d60aee67c7ebb480d8045b5d0
Luba-web <ldvasileva87@yandex.ru>
2023-06-16 16:52:24 +0300

    feature page Home

---
+++ frontend/src/Pages/Home/Advantages/Advantages.jsx
@@ -0,0 +1,11 @@
+import './Advantages.scss';
+
+export default function Advantages({ title, description, icon }) {
+  return (
+    <li className="advantages">
+      <img src={icon} alt={title} />
+      <h3 className="advantages__text-bold">{title}</h3>
+      <p className="advantages__text">{description}</p>
+    </li>
+  );
+}
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,10 +2,15 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './Home.scss';
 import InfoPopup from '../../Components/InfoPopup/InfoPopup';
-import { toggleInfoPopup } from '../../store/slices/togglePopupSlice';
+import { toggleInfoPopup, toggleRegisterPopup } from '../../store/slices/togglePopupSlice';
 import СonfirmationPasswordChange from '../../Components/СonfirmationPasswordChange/СonfirmationPasswordChange';
 import { getUser } from '../../store/slices/userSlice';
 import Footer from '../../Components/Footer/Footer';
+import Button from '../../ui/Button/Button';
+import { arrAdvantages, arrCardWorks, arrInitFQAHome } from '../../utils/consts';
+import CardQuestion from '../../Components/CardQuestion/CardQuestion';
+import Advantages from './Advantages/Advantages';
+import CardWork from './CardWork/CardWork';
 
 export default function Main({ children }) {
   const dispatch = useDispatch();
@@ -23,9 +28,71 @@ export default function Main({ children }) {
     dispatch(toggleInfoPopup(false));
   };
 
+  const handleRegisterClick = () => {
+    dispatch(toggleRegisterPopup(true));
+  };
+
   return (
     <section className="home-page">
       {children}
+      <div className="home-page__container-title">
+        <h1 className="home-page__title">Контроль расходов «Семейный бюджет»</h1>
+        <p className="home-page__description">
+          Отслеживай свои расходы и будь в курсе своего финансового состояния
+        </p>
+
+        <Button
+          variant="primary"
+          content="text"
+          text="Зарегистрироваться"
+          size="large"
+          onClick={handleRegisterClick}
+        />
+      </div>
+
+      <div className="home-page__img" />
+      <h3 className="home-page__subtitle">Преимущества</h3>
+      <ul className="home-page__lists-advantages">
+        {arrAdvantages.map((i) => {
+          return (
+            <Advantages title={i.title} description={i.description} icon={i.icon} key={i.id} />
+          );
+        })}
+      </ul>
+      <div className="home-page__container-how-works">
+        <h3 className="home-page__subtitle">Как это работает</h3>
+        <ul className="home-page__lists-how-works">
+          {arrCardWorks.map((i) => {
+            return (
+              <CardWork
+                title={i.title}
+                description={i.description}
+                img={i.img}
+                key={i.id}
+                number={i.number}
+              />
+            );
+          })}
+        </ul>
+      </div>
+      <div className="home-page__container-fqa">
+        <h3 className="home-page__text-bold home-page__text-start">Часто задаваемые вопросы</h3>
+        <ul>
+          {arrInitFQAHome.map((q) => {
+            return <CardQuestion question={q.question} answer={q.answer} key={q.id} />;
+          })}
+        </ul>
+        <h3 className="home-page__text-bold home-page__text-start">
+          Готовы начать контролировать бюджет?
+        </h3>
+        <Button
+          variant="primary"
+          content="text"
+          text="Начать"
+          size="large"
+          onClick={handleRegisterClick}
+        />
+      </div>
       {isInfoPopupOpen && (
         <InfoPopup
           onClose={closeInfoPopup}
@@ -33,7 +100,7 @@ export default function Main({ children }) {
           title="Пароль успешно изменен"
         />
       )}
-      <Footer extraClass="footer-absolute" />
+      <Footer extraClass="footer" />
     </section>
   );
 }
