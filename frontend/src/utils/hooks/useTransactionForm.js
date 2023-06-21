888bfc5a9305075c30f8119c45b32d66f9f856e1
Masha Moreva <mashamiskova@yandex.ru>
2023-06-21 19:49:22 +0300

    add-initial-transaction-state

--- frontend/src/utils/hooks/useTransactionForm.js
+++ frontend/src/utils/hooks/useTransactionForm.js
@@ -2,21 +2,21 @@ import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import useDropdown from './useDropdown';
 
-const useTransactionForm = (categoryType) => {
+const useTransactionForm = (initialTransaction, categoryType) => {
   const { finance, categories } = useSelector((state) => ({
     finance: state.userFinance.finance,
     categories: state.categories.categories,
   }));
 
-  const categoryDropdown = useDropdown(categories?.[0]?.id || '', categories);
-  const financeDropdown = useDropdown(finance?.[0]?.id || '', finance);
+  const categoryDropdown = useDropdown(initialTransaction?.category?.id || '', categories);
+  const financeDropdown = useDropdown(initialTransaction?.finance?.id || '', finance);
 
   const [formData, setFormData] = useState({
-    created: '',
-    category: '',
-    name: '',
-    amount: '',
-    finance: '',
+    created: initialTransaction?.created || '',
+    category: initialTransaction?.category?.id || '',
+    name: initialTransaction?.name || '',
+    amount: initialTransaction?.amount || '',
+    finance: initialTransaction?.finance?.id || '',
   });
 
   useEffect(() => {
