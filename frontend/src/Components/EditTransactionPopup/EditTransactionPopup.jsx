35a2f185be3ff18b015ef2c1512dbe54cd54cca3
Masha Moreva <mashamiskova@yandex.ru>
2023-06-21 19:49:56 +0300

    update-edit-popup-with-hook

--- frontend/src/Components/EditTransactionPopup/EditTransactionPopup.jsx
+++ frontend/src/Components/EditTransactionPopup/EditTransactionPopup.jsx
@@ -1,111 +1,57 @@
+import { useDispatch } from 'react-redux';
 import Popup from '../Popup/Popup';
-import Button from '../../ui/Button/Button';
+import TransactionForm from '../TransactionForm/TransactionForm';
+import { addTransaction, getTransactionList } from '../../store/slices/transactionList';
+import useTransactionForm from '../../utils/hooks/useTransactionForm';
 
-export default function EditTransactionPopup({ onClose, transaction }) {
+export default function EditTransactionPopup({ onClose, transaction, categoryType }) {
+  const dispatch = useDispatch();
+  const { formData, handleChange, categoryOptions, financeOptions } = useTransactionForm(
+    transaction,
+    categoryType,
+  );
+
   if (!transaction) {
     return null;
   }
 
-  function handleSubmit(evt) {
-    evt.preventDefault();
-  }
+  const selectedCategoryOption = categoryOptions.find(
+    (option) => option.id === transaction.category.id,
+  );
+  const selectedFinanceOption = financeOptions.find(
+    (option) => option.id === transaction.finance.id,
+  );
 
-  const handleCancel = () => {
-    onClose();
+  const handleAddSpend = (evt) => {
+    evt.preventDefault();
+    dispatch(addTransaction({ ...formData, category_type: transaction.category_type })).then(() => {
+      dispatch(getTransactionList());
+      onClose();
+    });
   };
 
   return (
-    <Popup onClose={onClose} popupSize="popup_edit-operation" title={transaction.name}>
-      <form className="form" onSubmit={handleSubmit}>
-        <p className="form__input-label">{transaction.category.name}</p>
-        <div className="form__input-block">
-          <label className="form__input-label" htmlFor="EditTransactionPopup-date">
-            Дата
-            <input
-              id="EditTransactionPopup-date"
-              name="created"
-              className="form__input form__input_date"
-              type="text"
-              placeholder="Введите дату"
-              defaultValue={new Date(transaction.created).toLocaleDateString('ru-RU')}
-            />
-          </label>
-        </div>
-
-        <div className="form__input-block">
-          <label className="form__input-label" htmlFor="SpendingPopup-category">
-            Категория расхода
-            <select
-              className="form__input form__input_select"
-              type="select"
-              name="SpendingPopup-category"
-              id="SpendingPopup-category"
-            >
-              <option value="" className="form__input_option">
-                Выбрать категорию
-              </option>
-            </select>
-          </label>
-        </div>
-
-        <div className="form__input-block">
-          <label className="form__input-label" htmlFor="EditTransactionPopup-name">
-            Название
-            <input
-              id="EditTransactionPopup-name"
-              name="name"
-              className="form__input"
-              type="text"
-              placeholder="Введите название"
-              defaultValue={transaction.name}
-            />
-          </label>
-        </div>
-
-        <div className="form__input-block">
-          <label
-            className="form__input-label form__input-label_divider"
-            htmlFor="EditTransactionPopup-amount"
-          >
-            Сумма
-            <input
-              id="EditTransactionPopup-amount"
-              name="amount"
-              className="form__input form__input_sum"
-              type="number"
-              placeholder="Введите сумму"
-              defaultValue={transaction.amount}
-            />
-          </label>
-        </div>
-
-        <div className="form__input-block">
-          <label className="form__input-label" htmlFor="SpendingPopup-card">
-            Счёт списания
-            <select
-              className="form__input form__input_select"
-              type="select"
-              name="SpendingPopup-card"
-              id="SpendingPopup-card"
-            >
-              <option value="" className="form__input_option">
-                Внести счёт
-              </option>
-            </select>
-          </label>
-        </div>
-
-        <div className="form__button-wrapper form__button-wrapper_add-operation">
-          <Button
-            variant="secondary"
-            content="text"
-            text="Отменить"
-            size="medium"
-            onClick={handleCancel}
-          />
-          <Button type="submit" variant="primary" content="text" text="Готово" size="medium" />
-        </div>
-      </form>
+    <Popup
+      onClose={onClose}
+      popupSize="popup_edit-operation"
+      title={transaction.name}
+      subtitle={transaction.category.name}
+    >
+      <TransactionForm
+        formData={{
+          created: new Date(transaction.created).toLocaleDateString('ru-RU'),
+          category: selectedCategoryOption ? selectedCategoryOption.id : '',
+          name: transaction.name,
+          amount: transaction.amount,
+          finance: selectedFinanceOption ? selectedFinanceOption.id : '',
+        }}
+        handleChange={handleChange}
+        handleSubmit={handleAddSpend}
+        categoryOptions={categoryOptions}
+        financeOptions={financeOptions}
+        onClose={onClose}
+        categoryType={transaction.category_type}
+      />
     </Popup>
   );
 }
