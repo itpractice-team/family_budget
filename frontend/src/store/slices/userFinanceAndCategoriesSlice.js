f16d1c115139fdf9b6834ad8926d1e4e6382baee
Masha Moreva <mashamiskova@yandex.ru>
2023-06-25 17:11:49 +0300

    add-new-actions

--- frontend/src/store/slices/userFinanceAndCategoriesSlice.js
+++ frontend/src/store/slices/userFinanceAndCategoriesSlice.js
@@ -1,5 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getUserCategoriesAPI, getUserFinanceAPI } from '../../utils/api';
+import {
+  getUserCategoriesAPI,
+  getUserFinanceAPI,
+  addFinanceAPI,
+  addCategoryAPI,
+} from '../../utils/api';
 
 export const getUserCategories = createAsyncThunk(
   'userFinanceAndCategories/getUserCategories',
@@ -12,6 +17,20 @@ export const getUserFinance = createAsyncThunk(
   'userFinanceAndCategories/getUserFinance',
   async () => {
     return getUserFinanceAPI();
+  },
+);
+
+export const addFinance = createAsyncThunk(
+  'userFinanceAndCategories/addFinance',
+  async (formData) => {
+    return addFinanceAPI(formData);
+  },
+);
+
+export const addCategory = createAsyncThunk(
+  'userFinanceAndCategories/addCategory',
+  async (formData) => {
+    return addCategoryAPI(formData);
   },
 );
 
@@ -49,6 +68,31 @@ export const userFinanceAndCategoriesSlice = createSlice({
         state.userFinance = action.payload.results;
       })
       .addCase(getUserFinance.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(addFinance.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addFinance.fulfilled, (state, action) => {
+        state.loading = false;
+
+        state.userFinance.push(action.payload);
+      })
+      .addCase(addFinance.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(addCategory.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addCategory.fulfilled, (state, action) => {
+        state.loading = false;
+        state.userCategories.push(action.payload);
+      })
+      .addCase(addCategory.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       });
