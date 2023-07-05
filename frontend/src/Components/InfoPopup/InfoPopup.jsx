f59c4d9659977892877775e68256e6cd43368a0f
Masha Moreva <mashamiskova@yandex.ru>
2023-07-05 11:51:04 +0300

    logic-in-progress

--- frontend/src/Components/InfoPopup/InfoPopup.jsx
+++ frontend/src/Components/InfoPopup/InfoPopup.jsx
@@ -1,5 +1,6 @@
 import './InfoPopup.scss';
 import Popup from '../Popup/Popup';
+import Button from '../../ui/Button/Button';
 import errorIcon from '../../Images/error.svg';
 import successIcon from '../../Images/success.svg';
 
@@ -9,17 +10,22 @@ const infoPopupData = [
     iconSrc: errorIcon,
     text: 'Ошибка',
     textClassName: 'info-popup__text_error',
+    textButton: 'Попробовать снова',
   },
   {
     type: 'success',
     iconSrc: successIcon,
-    message: 'Готово!',
+    text: 'Готово!',
     textClassName: 'info-popup__text_success',
+    textButton: 'Отлично!',
   },
 ];
 
-export default function InfoPopup({ onClose, content, title, subtitle, type }) {
-  const { iconSrc, text, textClassName } = infoPopupData.find((data) => data.type === type) || {};
+export default function InfoPopup({ onClose, content, title, subtitle, type, onClick }) {
+  const { iconSrc, text, textClassName, textButton } =
+    infoPopupData.find((data) => data.type === type) || {};
+
+  const handleClick = () => (type === 'success' ? onClose() : onClick());
 
   return (
     <Popup onClose={onClose} popupSize="popup_info" title={title} subtitle={subtitle}>
@@ -30,6 +36,14 @@ export default function InfoPopup({ onClose, content, title, subtitle, type }) {
         </div>
         <div className="info-popup__content">{content}</div>
       </div>
+      <Button
+        type="submit"
+        variant="primary"
+        content="text"
+        text={textButton}
+        size="medium"
+        onClick={handleClick}
+      />
     </Popup>
   );
 }
