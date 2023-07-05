d3a4b90353a0b36a9e220976288d454b07ed7498
Masha Moreva <mashamiskova@yandex.ru>
2023-07-05 11:49:52 +0300

    add-validation

--- frontend/src/Components/AddMoneyboxPopup/AddMoneyboxPopup.jsx
+++ frontend/src/Components/AddMoneyboxPopup/AddMoneyboxPopup.jsx
@@ -1,36 +1,29 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+/* eslint-disable no-shadow */
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useDispatch } from 'react-redux';
 import Popup from '../Popup/Popup';
 import Button from '../../ui/Button/Button';
 import { addMoneybox } from '../../store/slices/moneybox';
-import InfoPopup from '../InfoPopup/InfoPopup';
 import CancelButton from '../CancelButton/CancelButton';
 import usePopup from '../../utils/hooks/usePopup';
+import addMoneyboxValidation from '../../utils/validations/addMoneyboxValidation';
 
 export default function AddMoneyboxPopup({ onClose }) {
   const dispatch = useDispatch();
-  const { error } = useSelector((state) => state.moneybox);
-  const {
-    isOpen: isInfoPopupOpen,
-    openPopup: openInfoPopup,
-    closePopup: closeInfoPopup,
-  } = usePopup('info');
 
-  const [formData, setFormData] = useState({
-    name: '',
-    amount: '',
-    accumulated: 0,
-    description: '',
+  const { openPopup: openInfoPopup } = usePopup('info');
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(addMoneyboxValidation),
+    mode: 'onChange',
   });
 
-  const { name, amount, description } = formData;
-
-  const handleChange = (evt) => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value });
-  };
-
-  const handleAddMoneybox = (evt) => {
-    evt.preventDefault();
+  const onSubmit = (formData) => {
     dispatch(addMoneybox(formData)).then(() => {
       onClose();
       openInfoPopup();
@@ -38,59 +31,61 @@ export default function AddMoneyboxPopup({ onClose }) {
   };
 
   return (
-    <>
-      <Popup onClose={onClose} popupSize="popup_s" title="Задайте свою цель для накопления">
-        <form className="form" onSubmit={handleAddMoneybox}>
-          <label className="form__input-label" htmlFor="Moneybox-name">
-            Название конверта
+    <Popup onClose={onClose} popupSize="popup_s" title="Задайте свою цель для накопления">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <label className="form__input-label" htmlFor="Moneybox-name">
+          Название конверта
+          <input
+            id="Moneybox-name"
+            {...register('name')}
+            className={`form__input ${errors.name ? 'error' : ''}`}
+            type="text"
+            placeholder="Введите название"
+          />
+          {errors.name && (
+            <span className="form__valid-message form__valid-message_active">
+              {errors.name.message}
+            </span>
+          )}
+        </label>
+        <div className="form__input-block">
+          <label className="form__input-label form__input-label_divider" htmlFor="Moneybox-amount">
+            Итоговая сумма
             <input
-              id="Moneybox-name"
-              name="name"
-              className="form__input"
-              type="text"
-              placeholder="Введите название"
-              value={name}
-              onChange={handleChange}
+              id="Moneybox-amount"
+              {...register('amount')}
+              className={`form__input form__input_sum ${errors.amount ? 'error' : ''}`}
+              type="number"
+              placeholder="Введите сумму для накопления"
             />
+            {errors.amount && (
+              <span className="form__valid-message form__valid-message_active">
+                {errors.amount.message}
+              </span>
+            )}
           </label>
-          <div className="form__input-block">
-            <label
-              className="form__input-label form__input-label_divider"
-              htmlFor="Moneybox-amount"
-            >
-              Итоговая сумма
-              <input
-                id="Moneybox-amount"
-                name="amount"
-                className="form__input form__input_sum"
-                type="number"
-                placeholder="Введите сумму для накопления"
-                value={amount}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <label
-            className="form__input-label form__input-label_textarea"
-            htmlFor="Moneybox-comment"
-          >
-            Комментарий
-            <textarea
-              id="Moneybox-comment"
-              name="description"
-              className="form__input form__input_textarea"
-              placeholder="Заметка о цели"
-              value={description}
-              onChange={handleChange}
-            />
-          </label>
-          <div className="form__button-wrapper form__button-wrapper_add-operation">
-            <CancelButton onClose={onClose} />
-            <Button type="submit" variant="primary" content="text" text="Сохранить" size="medium" />
-          </div>
-        </form>
-      </Popup>
-      {isInfoPopupOpen && <InfoPopup onClose={closeInfoPopup} content={error} type="error" />}
-    </>
+        </div>
+        <label className="form__input-label form__input-label_textarea" htmlFor="Moneybox-comment">
+          Комментарий
+          <textarea
+            id="Moneybox-comment"
+            {...register('description')}
+            className="form__input form__input_textarea"
+            placeholder="Заметка о цели"
+          />
+        </label>
+        <div className="form__button-wrapper form__button-wrapper_add-operation">
+          <CancelButton onClose={onClose} />
+          <Button
+            type="submit"
+            variant="primary"
+            content="text"
+            text="Сохранить"
+            size="medium"
+            disabled={!isValid}
+          />
+        </div>
+      </form>
+    </Popup>
   );
 }
