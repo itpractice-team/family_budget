3a31ef6ccfe2247c0a5f270a254511d7fe89dc40
Luba-web <ldvasileva87@yandex.ru>
2023-07-16 09:44:35 +0300

    feature monthly-box

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,4 +1,4 @@
-export default function toISOString(s) {
+export function toISOString(s) {
   const months = {
     jan: '01',
     feb: '02',
@@ -16,4 +16,18 @@ export default function toISOString(s) {
   const b = s.split(' ');
 
   return `${b[3]}-${months[b[1].toLowerCase()]}-${`0${b[2]}`.slice(-2)}T${b[4]}${b[5].substr(3)}`;
+}
+
+export function arrCalendar() {
+  const array = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 1; i < 32; i++) {
+    array.push(i);
+  }
+  // eslint-disable-next-line prefer-const
+  let [list, chunkSize] = [array, 7];
+  // eslint-disable-next-line no-unused-vars
+  list = [...Array(Math.ceil(list.length / chunkSize))].map((_) => list.splice(0, chunkSize));
+
+  return list;
 }
